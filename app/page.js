f2fd8559325bd8728272ff3cc8b4b40011e6b57f
AbdullahUsmani2746 <abdullah.usmani.2746@gmail.com>
2025-02-17 23:13:16 +0500

    Changes in the code

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
+import { AppSidebar } from "@/components/sidebar/app-sidebar"
+import { SiteHeader } from "@/components/sidebar/site-header"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
 
-
-export default function Home() {
+export default function Home({children}) {
   return (
-    <div className='flex'></div>
+ <div className="[--header-height:calc(theme(spacing.14))]">
+        <SidebarProvider className="flex flex-col">
+          <SiteHeader />
+          <div className="flex flex-1">
+            <AppSidebar />
+            <SidebarInset>
+            {children}
+            </SidebarInset>
+            </div>
 
+          </SidebarProvider>
+          </div>
   );
 }
