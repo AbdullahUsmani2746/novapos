c3898aba741b26da541956e2baab55aba77d99da
basilmuhammad91 <basilmuhammad91@gmail.com>
2025-04-17 00:29:04 +0500

    =file added

---
+++ app/(pages)/(accounts)/show-company/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import EntityPageLayout from '@/components/shared/EntityPageLayout'
+
+export default function CompaniesPage() {
+  return (
+    <EntityPageLayout
+      title="Companies"
+      endpoint="companies"
+      fields={['company_id', 'company', 'addr1', 'addr2', 'city', 'phone', 'fax', 'email']}
+    />
+  )
+}
