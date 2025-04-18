657964caf466787b727f3d11186ffd9ea52573ff
basilmuhammad91 <basilmuhammad91@gmail.com>
2025-04-19 00:07:29 +0500

    =fixed ids

--- components/shared/EntityPageLayout.jsx
+++ components/shared/EntityPageLayout.jsx
@@ -34,7 +34,7 @@ const EntityPageLayout = ({ title, endpoint, fields, buttonText = null }) => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
     })
-    
+
 
     setEditingItem(null)
     fetchData()
